a81c6ee44db3fd69655ccad075d2ff23d2241422
SarahGormley <sarah.gormley39@gmail.com>
2022-03-28 12:10:26 +1000

    Adding variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var chracters = [
+    numbers = "1234567890"
+    lowLetters = "abcdefghijklmnopqrstuvwxyz"
+    upLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    characters = " !#$%&'()*+,-./:;<=>?@[]_`{|}~"
+]
+var passwordLength = window.prompt("How long would you like your password to be?")
+var lowerCase = window.confirm("Do you want lowercase letters?")
+var upperCase = window.confirm("Do you want uppercase letters?")
+var integ = window.confirm("Do you want numbers?")
+var specChars = window.confirm("Do you want special characters?")
+
+if (!lowerCase && !upperCase && !integ && !specChars) {
+    alert("Please select atleast one character type!");
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
