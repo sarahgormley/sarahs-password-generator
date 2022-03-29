e783a842391891689649288a67441b393c456694
SarahGormley <sarah.gormley39@gmail.com>
2022-03-29 17:50:34 +1000

    Event listener working

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var enter;
+// Variables for User Input
 var yesNumbers;
 var yesSymbols;
 var yesLowerCase;
 var yesUppercase;
-//symbols generator
+
 // Special characters 
 symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 // Numeric characters
@@ -14,12 +14,8 @@ number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-
 // Write password to the #password input
 function writePassword() {
-    //var password = "" //generatePassword();
-    // var passwordText = document.querySelector("#password");
-    // var passwordCharSet = "";
     var passwordLength = window.prompt("How long would you like your password to be? (Between 8-128)")
     if (!passwordLength) {
         alert("Please enter a password length between 8 - 128!");
@@ -31,7 +27,7 @@ function writePassword() {
         yesLowerCase = window.confirm("Do you want lowercase letters?");
         yesUpperCase = window.confirm("Do you want uppercase letters?");
     }
-    // If no options are chosem
+    // If no options are chosen
     if (!yesLowerCase && !yesUpperCase && !yesNumbers && !yesSymbols) {
         alert("Please select atleast one character type!");
         // If all 4 options are chosen
@@ -71,12 +67,14 @@ function writePassword() {
     } else if (yesUppercase) {
         choices = upperCase
     }
+    // Variable for password- left blank based on user choices
     var password = [];
-
-    for (var i = 0; i < enter; i++) {
+    // Random selection of variables
+    for (var i = 0; i < passwordLength; i++) {
         var pickChoices = choices[Math.floor(Math.random() * choices.length)];
         password.push(pickChoices);
     }
+    //Saves choice as password
     var pword = password.join("");
     UserInput(pword);
     return pword;
@@ -84,11 +82,8 @@ function writePassword() {
 
 function UserInput(pword) {
     document.getElementById("password").textContent = pword;
-
+    console.log(pword)
 }
-// Add event listener to generate button
+
+// Add event listener to generate button - Shows password on click.
 generateBtn.addEventListener("click", writePassword)
-generateBtn.addEventListener("click", function() {
-    ps = writePassword();
-    document.getElementById("password").placeholder = ps;
-});
